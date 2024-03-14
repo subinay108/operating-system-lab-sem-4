d73730aec1be46fcd8b351cc6febd6bd12ceed9d
= <subinayp108@gmail.com>
2024-03-14 18:56:27 +0530

    rr

--- roundRobin.c
+++ roundRobin.c
@@ -28,8 +28,57 @@ void dequeue(){
 	}
 }
 
+void swap(int* a, int* b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 int main()
 {
+	int n;
+	printf("Enter the no. of process: ");
+	scanf("%d", &n);
+	int i;
+	int pid[n], at[n], bt[n], ct[n], tat[n], wt[n], rt[n];
+
+
+	// input process ids of each process
+	printf("Enter the process id of all the processes: ");
+	for(i = 0; i < n; i++){
+		scanf("%d", &pid[i]);
+	}
+
+	// input arrival time and burst time of each processes
+	for(i = 0; i < n; i++){
+		printf("Enter the arrival time and burst time of process with id = p%d: ", pid[i]);
+		scanf("%d %d", &at[i], &bt[i]);
+	}
+
+	// Step1: Sort the process with respect to arrival time
+	int min = 0;
+	int j;
+	for(i = 0; i < n - 1; i++){
+		min = i;
+		for(j = i + 1; j < n; j++){
+			if(at[min] > at[j]){
+				min = j;
+			}
+		}
+		// swap i and min of at
+		swap(&at[i], &at[min]);
+
+		// swap i and min of pid
+		swap(&pid[i], &pid[min]);
+
+		// swap i and min of bt
+		swap(&bt[i], &bt[min]);
+	}
+
+	// Step2: 
+
+
+
 	
 	return 0;
 }
