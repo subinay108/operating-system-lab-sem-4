5d3838533a8630b674fc70de8b066b1c3e51f70c
subinay108 <subinayp108@gmail.com>
2024-04-18 14:33:10 +0530

    lab-6 signal.c is updated

--- Lab-6/signal.c
+++ Lab-6/signal.c
@@ -1,8 +1,35 @@
-#include<stdio.>
+// Write a Program on Signal handling, sending singnals, signal interrupt
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
 #include<signal.h>
 
-int main(int argc, char **argv){
-    void InterruptHandler(), InitHandler(), n = 0;
-    signal(SIGINT, InterruptHandler);
-    printf("This is not required!");
+void sig_handler(int signum){
+    printf("Received signal %d\n", signum);
+}
+
+int main(){
+    if(signal(SIGINT, sig_handler) == SIG_ERR){
+        perror("Signal registration failed");
+        return 1;
+    }
+    if(signal(SIGTERM, sig_handler) == SIG_ERR){
+        perror("Signal registration failed");
+        return 1;
+    }
+    printf("Sending SIGINT\n");
+    kill(getpid(), SIGINT);
+    sleep(2);
+    printf("Sending SIGTERM\n");
+    kill(getpid(), SIGTERM);
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGUSR1);
+    if(sigismember(&set, SIGUSR1)){
+        printf("SIGUSR1 is in the set\n");
+    }else{
+        printf("SIGUSR1 is not in the set\n");
+    }
+
+    return 0;
 }
